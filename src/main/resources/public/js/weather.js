5f89f4801235b27bc15ec0fa0c5ec85278d509af
Olga Kleptsova <okl.yep@gmail.com>
2019-08-12 11:06:40 +0500

    upd front

--- src/main/resources/public/js/weather.js
+++ src/main/resources/public/js/weather.js
@@ -1,44 +1,47 @@
-function updateWeather() {
-    $.ajax({
-        url: "/weather?city=" + encodeURI(localStorage.city) + '&weatherService=' + encodeURI(localStorage.weatherService),
-        success: function (data) {
-            $("#temp").html(data["temp"]);
-            $("#wind").html(data["wind"]);
-            $("#humidity").html(data["humidity"]);
-            $("#error").hide();
-        },
-        error: function () {
-            $("#error").show();
-        }
-    })
-}
-
-var i = 0;
-
-function setupSelect(element, data) {
-    Object.keys(data).forEach(function (value) {
-        element.append("<option value='" + value + "'>" + data[value] + "</option>")
-    });
-    element.bind("change", function () {
-        localStorage[element.attr("id")] = this.value;
-        updateWeather();
-    });
-    if (element.find("option[value='" + localStorage[element.attr("id")] + "']").length > 0)
-        element.val(localStorage[element.attr("id")]);
-    else {
-        var val = element.find("option")[0].value;
-        element.val(val);
-        localStorage[element.attr("id")] = val;
-    }
-    if (i > 0) updateWeather();
-    else i++;
-}
-
 $(document).ready(function () {
-    $.ajax({url: "/cities"}).then(function (data) {
-        setupSelect($("#city"), data)
-    });
-    $.ajax({url: "/weatherServices"}).then(function (data) {
-        setupSelect($("#weatherService"), data)
-    });
+    function updateWeather() {
+        $.ajax({
+            url: "/weather?city=" + encodeURI(localStorage.city) + '&weatherService=' + encodeURI(localStorage.weatherService),
+            success: function (data) {
+                $("#temp").html(data["temp"]);
+                $("#wind").html(data["wind"]);
+                $("#humidity").html(data["humidity"]);
+                $("#error").hide();
+            },
+            error: function () {
+                $("#error").show();
+            }
+        })
+    }
+
+    var loaded = 0;
+
+    $.each([["/cities", $("#city")], ["/weatherServices", $("#weatherService")]],
+        function (index, value) {
+            $.ajax({
+                url: value[0],
+                success: function (data) {
+                    var element = value[1];
+                    Object.keys(data).forEach(function (value) {
+                        element.append("<option value='" + value + "'>" + data[value] + "</option>")
+                    });
+                    element.bind("change", function () {
+                        localStorage[element.attr("id")] = this.value;
+                        updateWeather();
+                    });
+                    if (element.find("option[value='" + localStorage[element.attr("id")] + "']").length > 0)
+                        element.val(localStorage[element.attr("id")]);
+                    else {
+                        var val = element.find("option")[0].value;
+                        element.val(val);
+                        localStorage[element.attr("id")] = val;
+                    }
+                    loaded++;
+                }
+            }).then(function setupSelect() {
+                    if (loaded >= 2)
+                        updateWeather();
+                }
+            )
+        });
 });
